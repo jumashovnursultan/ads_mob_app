d18808f40f93834d57654b6517b7328889e694f3
nursultanjumashov <nursultanjumashovv@gmail.com>
2023-08-21 23:34:36 +0600

    bottomNavigationBar

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -9,10 +9,35 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: TextButton(
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-          },
-          child: const Text('LogOut')),
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+        },
+        child: const Text('LogOut'),
+      ),
+      bottomNavigationBar: Container(
+        height: 50,
+        width: 50,
+        margin: const EdgeInsets.only(bottom: 25, left: 16, right: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 8),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(Icons.campaign),
+            Icon(Icons.account_box),
+          ],
+        ),
+      ),
     );
   }
 }
